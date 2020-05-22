29ba8456f2c5faf40e5b8deed3b0b9e29a552bbe
Mikel <mikel.agirrebeitia@alumni.mondragon.edu>
2020-05-22 10:39:57 +0200

    Modificamos la variable Nif en la rama correpondiente

--- Carga fichero.R
+++ Carga fichero.R
@@ -6,3 +6,10 @@ str(muestra01)
 #Cambiamos los nombres de las variables
 names(muestra01)[c(1,2,3)]<- c("nif", "nombre", "genero")
 colnames(muestra01)
+
+library(stringr)
+
+#nos quedamos con las instancias con 8 digitos y una letra
+muestra01<- muestra01[str_detect(muestra01[,1], "([[:digit:]]{8})([[:alpha:]]{1})")==T,]
+dim(muestra01)
+#304832 filas y 3 columnas
