e5f21b0754fb10e5a31a8a610c5d756768f75442
Ander <ander.garaib@alumni.mondragon.edu>
2020-05-22 10:08:09 +0200

    modificamos R

---
+++ Cargadedatos.R
@@ -0,0 +1,8 @@
+load("C:/Users/2001g/Desktop/business data analytics/GITHUB/definitiva/Archivos_examen_ds/muestra18.RData")
+str(muestra18)
+dim(muestra18)
+
+names(muestra18)[1] = "nif"
+names(muestra18)[2] = "nombre"
+names(muestra18)[3] = "genero"
+
