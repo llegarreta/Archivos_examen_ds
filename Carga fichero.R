6a530a94f63f78a772e594d9469949f13c064d01
Janire <janire.amesti@alumni.mondragon.edu>
2020-05-22 09:09:10 +0200

    Modificamos el nombre de las columnas

--- Carga fichero.R
+++ Carga fichero.R
@@ -3,3 +3,5 @@ load("muestra04.RData")
 dim(muestra04)
 #en las variables tenemos 3 columnas: un código, nombres y género.
 #las dimensiones del archivo son 332086 x 3  (3 columnas 332086 instancias)
+
+names(muestra04)<-c("nif","nombre","genero")
