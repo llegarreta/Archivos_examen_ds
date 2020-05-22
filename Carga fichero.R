769732b846ba3f76b04a66aaf87bd3c65584a39a
Iker <iker.cumplido@alumni.mondragon.edu>
2020-05-22 10:09:46 +0200

    Modificamos genero

--- Carga fichero.R
+++ Carga fichero.R
@@ -17,3 +17,53 @@ str(muestra14)
 # Modificar nombres -------------------------------------------------------
 
 colnames(muestra14)<-c("nif","nombre","genero")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# Modificaciones de género--------------------------------
+#hemos dejado filas libres para mergear bien
+
+library(dplyr)
+
+table(muestra14$genero)
+
+#eliminamos "E" y "X" porque no están bien informadas
+muestra14<-muestra14%>%
+  filter(genero!="E")%>%
+  filter(genero!="X")
+
+dim(muestra14) #hemos eliminado 30 filas
+
+library(tidyverse)
+tablafrecuencias<-muestra14[,c(2,3)]
+tablafrecuencias<-tablafrecuencias[tablafrecuencias$genero!="",]
+
+chicos<-tablafrecuencias%>%
+  filter(genero=="M")
+
+chicas<-tablafrecuencias%>%
+  filter(genero=="V")
+
+
