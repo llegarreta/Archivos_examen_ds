d14fa79629fa63fdc8d45a0c27d09c51c29466e5
maria <mariaepelde8@gmail.com>
2020-05-22 09:12:46 +0200

    hemos cambiado el nombre de nuestras variables

--- Carga fichero.R
+++ Carga fichero.R
@@ -1,5 +1,9 @@
 
 
 # EXAMEN DS ---------------------------------------------------------------
-
-
+directorio <- setwd('C:/Users/auror/Desktop/RETOS/RETO 4/DS/Examen_DS/Archivos_examen_ds')
+load('muestra17.RData')
+dim(muestra17)
+colnames(muestra17) 
+str(muestra17)
+#El fichero tiene 310110 registros y 3 variables
