6712a972e7c69bd9ba9236249bf317953e64b05e
Iker Nieto <iker.nieto@alumni.mondragon>
2020-05-22 10:29:37 +0200

    Completamos todos los nif

--- Carga fichero.R
+++ Carga fichero.R
@@ -4,3 +4,14 @@ colnames(muestra30)
 names(muestra30)
 names(muestra30)<-c('nif','nombre','género')
 
+#Creo el patrón que cumplen los nif 8 digitos y 1 letra
+patron = "([[:digit:]]{8})([[:alpha:]]{1})"
+
+#con str_extract saco las combinaciones que coincidan
+a=str_extract(muestra30$nif, patron)
+
+#Lo añado a dataframe
+a=data.frame(a)
+
+#Uno la tabla a del patron a la muestra original
+muestra30=inner_join(a, muestra30, by = c("a" = "nif"))
