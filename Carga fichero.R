fb5f8352dc0d6e41e7818f21796567320eb53f96
Irene <ireneherrera@alumni.mondragon.edu>
2020-05-22 09:21:10 +0200

    Modificamos el fichero Carga fichero.R

---
+++ Carga fichero.R
@@ -0,0 +1,15 @@
+setwd("C:/Users/USUARIO/OneDrive/MU/MATERIAS/DATA SCIENCE I/Examen_DS/Archivos_examen_ds")
+
+df<-load("muestra22.RData")
+
+#Las dimensiones de Muestra22 son 302711 x 3. Tenemos tres variables var1, var2 y var 3
+dim(muestra22)
+str(muestra22)
+
+
+
+
+
+
+
+
