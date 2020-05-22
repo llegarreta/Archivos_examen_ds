51bc9cf5100f3f6f4282b042eceab2517814f7c0
Irene <ireneherrera@alumni.mondragon.edu>
2020-05-22 10:06:18 +0200

    Modificamos carga de ficheros en la rama-nif

--- Carga fichero.R
+++ Carga fichero.R
@@ -5,11 +5,15 @@ df<-load("muestra22.RData")
 #Las dimensiones de Muestra22 son 302711 x 3. Tenemos tres variables var1, var2 y var 3
 dim(muestra22)
 str(muestra22)
+load("muestra22")
 
 names(muestra22)[1]<-"nif"
 names(muestra22)[2]<-"nombre"
 names(muestra22)[3]<-"genero"
 
+patata <- "([[:digit:]]{8})([[:alpha:]]{1})"
 
+patata1<-grepl(pattern = patata, x = muestra22$nif)#vemos que en los FALSE no se cumple la condicion que hemos puesto en patata
 
+str_replace(muestra22[,1],"([[:digit:]]{8})([[:alpha:]]{1})",TRUE)
 
