f091855f7cb9c3b2494811a8b1e90f1df5834ce7
Iker <iker.cumplido@alumni.mondragon.edu>
2020-05-22 09:36:40 +0200

    Modificamos la columna nif

--- Carga fichero.R
+++ Carga fichero.R
@@ -17,3 +17,33 @@ str(muestra14)
 # Modificar nombres -------------------------------------------------------
 
 colnames(muestra14)<-c("nif","nombre","genero")
+
+
+# Modificaciones en NIF ---------------------------------------------------
+
+library(dplyr)
+
+table(muestra14$nif)
+
+sum(is.na(muestra14$nif)) #hay una fila con NA
+dim(muestra14[muestra14$nif=="",]) #tiene 12 filas sin informar
+
+proporcion_sin_info<-dim(muestra14[muestra14$nif=="",])[1]/dim(muestra14)[1]
+proporcion_sin_info #la proporcion es muy pequeña así que podemos eliminar estos valores
+
+muestra14_informadas<-muestra14%>%   #hemos quitado los NAs y los valores sin informar
+  filter(nif!="")%>%
+  filter(is.na(nif)==F)
+dim(muestra14_informadas) #hemos eliminado 12 filas
+
+#vemos que hay varios usuarios con nif mal introducido (8 dígitos y una letra)
+#hay que eliminarlos
+
+regexp <- "([[:digit:]]{8})([[:alpha:]]{1})" #el patrón tiene que ser 8 dígitos y una letra
+bien<-grepl(pattern = regexp, x = muestra14$nif) #sale T o F dependiendo de si cumple o no
+bieninformadas<-muestra14[bien,]
+dim(bieninformadas) #2833306 filas y 3 columnas
+
+dim(muestra14)[1]-dim(bieninformadas)[1] #se han eliminado 28804 filas
+
+
