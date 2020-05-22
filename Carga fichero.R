fdead91c38ff13893d1418c976c4730124295bb8
Janire <janire.amesti@alumni.mondragon.edu>
2020-05-22 09:07:04 +0200

    Añadimos fichero carga de fichero

---
+++ Carga fichero.R
@@ -0,0 +1,5 @@
+setwd("C:/Users/Amesti/OneDrive - Mondragon Unibertsitatea/BDatA/Data science/Examen_DS/Archivos_examen_ds")
+load("muestra04.RData")
+dim(muestra04)
+#en las variables tenemos 3 columnas: un código, nombres y género.
+#las dimensiones del archivo son 332086 x 3  (3 columnas 332086 instancias)
