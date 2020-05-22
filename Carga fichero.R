a12a2f2e50f153c8b90ac33a5e09abd699a3035e
leire <leire.bartureni@alumni.mondragon.edu>
2020-05-22 10:25:01 +0200

    Procesamiento del genero

--- Carga fichero.R
+++ Carga fichero.R
@@ -10,3 +10,60 @@ names(muestra07)
 
 colnames(muestra07) <- c("nif", "nombre" , "genero")
 str(muestra07)
+
+# Procesamiento del genero ------------
+
+unique(muestra07$genero)
+library(dplyr)
+muestra07$genero2<-muestra07$genero
+unique(muestra07$genero2)
+muestra2<-muestra07 %>%
+  filter(muestra07$genero2=="" |muestra07$genero2=="X" |muestra07$genero2=="E")
+muestra2$genero2<-NA
+muestra07<-muestra07 %>%
+  filter(muestra07$genero2=="M" |muestra07$genero2=="V")
+muestra07<-rbind(muestra07,muestra2)
+
+sum(is.na(muestra07$genero2)) /nrow(muestra07)
+# El 59 % son NAs
+unique(muestra07$genero2)
+
+names(muestra07)[4]<-"genero_imp"
+
+mujeres<-muestra07 %>%
+  filter(genero_imp=="M")
+
+hombres<-muestra07 %>%
+  filter(genero_imp=="V")
+
+m<-mujeres %>%
+  group_by(nombre,genero_imp)%>%
+  summarise(n=n())
+
+h<-hombres %>%
+  group_by(nombre,genero_imp)%>%
+  summarise(n=n())
+comparacion<-m %>%
+  full_join(h, by="nombre", suffix=c(".m", ".h"))
+
+sum(is.na(comparacion$nombre))
+
+comparacion[which(is.na(comparacion$n.h)),][5]<-0
+comparacion[which(is.na(comparacion$n.m)),][3]<-0
+comparacion<-comparacion[,-4]
+m_b<-comparacion[which(comparacion$n.m>comparacion$n.h),]
+m_b$genero_imp.m<-"M"
+
+h_b<-comparacion[which(comparacion$n.h>comparacion$n.m),]
+h_b$genero_imp.m<-"V"
+
+ambos<-comparacion[which(comparacion$n.h==comparacion$n.m),]
+ambos$genero_imp.m<-"v" #criterio propio: he mirado los nombres
+
+corregido<-rbind(m_b,h_b, ambos)
+names(corregido)
+
+muestra07<-merge(muestra07,corregido, by="nombre")
+names(muestra07)
+muestra07<-muestra07[,-c(4,6,7)]
+names(muestra07) [4]<- "genero_imp"
