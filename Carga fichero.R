2fabab05724ed3d56fcc79399d2859b689ab0a44
Eneko <eneko.renteria@gmail.com>
2020-05-22 09:40:29 +0200

    Limpiamos la variable nif

--- Carga fichero.R
+++ Carga fichero.R
@@ -4,3 +4,10 @@ load('C:/Users/usuario/OneDrive/Escritorio/Examen_DS/Archivos_examen_ds/muestra3
  
 colnames(muestra36) <- c('nif', 'nombre', 'genero')
 
+#### NIF
+library(stringr)
+#Nos quedamos con 8 digitos al principio y 1 letra al final
+muestra36 <- muestra36[str_detect(muestra36$nif, "(^[:digit:]{8})([:alpha:]{1}$)"),]
+
+dim(muestra36)
+# El dataset muestra 36 ahora contiene: 306486 filas y 3 columnas (variables)
