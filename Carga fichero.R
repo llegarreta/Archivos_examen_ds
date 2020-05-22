245c764c28f1801dc51039cac7472b69f11fd229
alex <alejandro.santos@alumni.mondragon.edu>
2020-05-22 10:35:40 +0200

    He hecho procesamiento de nif, y ahora voy a hacer merge

--- Carga fichero.R
+++ Carga fichero.R
@@ -4,4 +4,22 @@ View(muestra39)
 names(muestra39)
 names(muestra39)<-c('nif','nombre','género')
 View(muestra39)
+library(stringi)
+library(stringr)
+library(rebus)
+library(dplyr)
 
+#como no sabía hacer lo del genero he cambiado a rama-nif y he empezado el procesamiento del nif
+
+#Examino el patron que sigur cada nif(es decir, 8 numeros y una letra)
+coincidencia = "([[:digit:]]{8})([[:alpha:]]{1})"
+
+#cojo las que coinciden
+df1=str_extract(muestra39$nif, coincidencia)
+
+#Lo añado a dataframe
+df1=data.frame(df1)
+
+#Uno la tabla a del patron a la muestra original
+muestra39=inner_join(df1, muestra39, by = c("df1" = "nif"))
+View(muestra39)
