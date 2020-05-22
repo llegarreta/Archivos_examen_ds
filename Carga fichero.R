29ce7a4721917fbbf14ae06ffa64d30df5768924
mikel j <mikel.juarez@alumni.mondragon.edu>
2020-05-22 09:28:27 +0200

    Creamos Carga ficheros.R y miramos las dimensiones

---
+++ Carga fichero.R
@@ -0,0 +1,4 @@
+setwd("C:/Users/mikel/OneDrive - Mondragon Unibertsitatea/MU - One Drive/BDA 1/Data Science/Git/Examen_DS/Archivos_examen_ds-master")
+load("C:/Users/mikel/OneDrive - Mondragon Unibertsitatea/MU - One Drive/BDA 1/Data Science/Git/Examen_DS/Archivos_examen_ds-master/muestra25.RData")
+
+dim(muestra25) #308084 3
