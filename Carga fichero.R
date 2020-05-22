4edc49719955e584f2f0d039dbbc848c352a9f76
Ane <ane.martinez@alumni.mondragon.edu>
2020-05-22 10:32:36 +0200

    Modificamos el nif

--- Carga fichero.R
+++ Carga fichero.R
@@ -9,3 +9,14 @@ View(muestra29)
 #cambiamos el nombre de  las columnas
 dput(colnames(muestra29))
 colnames(muestra29) <- c("nif", "nombre", "genero")
+
+#arreglamos la columna nif
+
+regexp <- "([[:digit:]]{8})([[:alpha:]]{1})"
+
+muestra29$nif_tf <- grepl(pattern = regexp, x = muestra29$nif)
+
+muestra29 <- muestra29%>%
+  filter(nif_tf==T)
+
+muestra29 <- muestra29[,-4]
