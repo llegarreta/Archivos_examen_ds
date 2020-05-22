0769a296b07f6105894346f9ac2b89117acee108
Unai Torrecilla <unai.torrecilla@alumni.mondragon.edu>
2020-05-22 10:10:04 +0200

    Analizo el NIF

--- Carga Fichero.R
+++ Carga Fichero.R
@@ -12,3 +12,4 @@ formato_correcto <- grepl('[[:digit:]]{8}[[:alpha:]]{1}', muestra40$nif)
 formato_correcto
 formato_correcto1 <- which(formato_correcto == T)
 df_nice <- muestra40[formato_correcto1, ]
+
