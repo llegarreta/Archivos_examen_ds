894049cd5e9407e2f1b82c6a8dd1e7b692400d69
Jon <jon.amelibia@alumni.mondragon.edu>
2020-05-22 09:57:13 +0200

    Modificamos los NIF

--- Carga fichero.R
+++ Carga fichero.R
@@ -9,3 +9,60 @@ View(muestra03)
 
 #Modificamos el nombre de las variables
 colnames(muestra03) <- c("NIF","NOMBRE", "GENERO")
+
+# imputaciones de genero --------------------------------------------------
+
+# creamos la columna
+
+muestra03$genero_imp <- NA
+
+# tratamos de ver la frecuencia de los nombres
+
+sort(table(muestra03$NOMBRE))
+options(max.print = 99999999)
+
+# para empezar con la imputación, los nombres de mujer más comunes, por ejemplo,
+# María, Ana, Cristina, Teresa y demás nombres, también hay compuestos finalizados en o que contienen
+# otros, normalmente María o Mari.
+
+library(stringr)
+
+# Primero los pondremos en mayúsculas
+muestra03$NOMBRE <-   toupper(muestra03$NOMBRE)
+
+# Pero antes intentaremos corregir los Jose´s porque puede haber conflicto con las Mari Jose´s
+# Primero probaremos con algunos nombres comunes de hombre
+muestra03$genero_imp[str_detect(muestra03$NOMBRE, pattern = "O$|EL$|JULIO|JOSE|CARLOS|DAVID|MANUEL|PABLO|PEDRO|JUAN|LUIS|CESAR")] <- "M"
+# Además de los finalizados por O (aunque podamos cargarnos alguno de mujer)
+
+# Añadimos Rosario y Rocio por lo del paso anterior
+muestra03$genero_imp[str_detect(muestra03$NOMBRE, pattern = "A$|AR$|MARIA|ANA|ROSARIO|ROCIO")] <- "F"
+
+# Muchos nombres vascos de chica acaban en NE
+muestra03$genero_imp[str_detect(muestra03$NOMBRE, pattern = "NE$|EN$")] <- "F"
+
+# Y los de chicos tienen tambien algunas terminaciones populares
+muestra03$genero_imp[str_detect(muestra03$NOMBRE, pattern = "ON$|OSU$|AITZ$|IKA$|ER$|OR$")] <- "M"
+
+length(muestra03$genero_imp)
+sum(is.na(muestra03$genero_imp))
+
+# Vemos que de 306085 entradas solo nos quedan sin imputar 58204, algo menos del 20%
+# pero tampoco hay que tomar mucho tiempo en este paso.
+
+
+
+# Imputaciones NIF --------------------------------------------------------
+
+muestra03$NIF <-   toupper(muestra03$NIF)
+muestra03$NIF[gsub("^[:alpha:]","",muestra03$NIF)]
+muestra03$NIF[!str_detect(muestra03$NIF, pattern = "[:digit:]{8}[:upper:]{1}")] <- NA
+muestra03$NIF
+
+library(dplyr)
+muestra03 <- muestra03 %>%
+  filter(is.na(NIF)==FALSE)
+
+dim(muestra03)
+
+# se nos queda una dimensión de 277771 filas y 4 columnas
