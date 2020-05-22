d15cf1d87f00ec0c28dc54043badf6afab167ba3
alex <alejandro.santos@alumni.mondragon.edu>
2020-05-22 09:30:50 +0200

    Commit de los nombres en R

---
+++ Carga fichero.R
@@ -0,0 +1,7 @@
+load("C:/Users/asaaa/Desktop/EXAMEN_DS1/Archivos_examen_ds/muestra39.RData")
+dim(muestra39)
+View(muestra39)
+names(muestra39)
+names(muestra39)<-c('nif','nombre','género')
+View(muestra39)
+
