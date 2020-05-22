f4c7c3cf090afad69be73eb791731f0a16752d56
Jie <jie.wu@alumni.mondragon.edu>
2020-05-22 09:10:59 +0200

    Creamos un scrpt en R y cargamos los datos en R

---
+++ Carga fichero.R
@@ -0,0 +1,10 @@
+setwd('C:/Users/jiewu/OneDrive/Escritorio/Examen_DS/Archivos_examen_ds')
+
+# Librerias ---------------------------------------------------------------
+library(stringr)
+# Carga de datos ----------------------------------------------------------
+load('muestra42.RData')
+
+
+# Mirar las dimensiones del fichero ---------------------------------------
+dim(muestra42) # Los datos tienen 300685filas y 3 variables
