5ef3b6a7ddb9acb2673b335d472edcc672e180e8
leire <leire.bartureni@alumni.mondragon.edu>
2020-05-22 09:15:21 +0200

    Modificamos nombre de las variables

--- Carga fichero.R
+++ Carga fichero.R
@@ -3,5 +3,10 @@ setwd("C:/Users/Leire/OneDrive - Mondragon Unibertsitatea/BDatA/Data Science/Int
 df <- load("muestra07.RData")
 
 dim(muestra07)
-
+str(muestra07)
 # Este fichero contiene 335086 filas y 3 columnas 
+
+names(muestra07)
+
+colnames(muestra07) <- c("nif", "nombre" , "genero")
+str(muestra07)
