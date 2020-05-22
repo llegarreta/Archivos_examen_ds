6484c31d48ddf489d47f423d1a15c35e15eb7ac9
Dani <daniel.puente@alumni.mondragon.edu>
2020-05-22 08:54:47 +0200

    vuelvo a icorporar los datos por si no se han guardado previamente

---
+++ Carga fichero.R
@@ -0,0 +1,9 @@
+
+load("C:/Users/Daniel Puente/Desktop/Examen_DS/Archivos_examen_ds/muestra35.RData")
+
+dim(muestra35)
+names(muestra35)[1] <- "nif"
+names(muestra35)[2] <- "Nombre"
+names(muestra35)[3] <- "genero"
+names(muestra35)
+
