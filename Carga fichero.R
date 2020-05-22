446c2c4e4f2fd5fbafadcb767b71eaf302edb61e
Jon <jon.amelibia@alumni.mondragon.edu>
2020-05-22 09:24:13 +0200

    Guardamos las imputaciones de genero

--- Carga fichero.R
+++ Carga fichero.R
@@ -9,3 +9,48 @@ View(muestra03)
 
 #Modificamos el nombre de las variables
 colnames(muestra03) <- c("NIF","NOMBRE", "GENERO")
+
+# imputaciones de genero --------------------------------------------------
+
+# creamos la columna
+
+muestra03$genero_imp <- NA
+
+# tratamos de ver la frecuencia de los nombres
+
+sort(table(muestra03$NOMBRE))
+options(max.print = 99999999)
+
+# para empezar con la imputación, los nombres de mujer más comunes, por ejemplo,
+# María, Ana, Cristina, Teresa y demás nombres, también hay compuestos finalizados en o que contienen
+# otros, normalmente María o Mari.
+
+library(stringr)
+
+# Primero los pondremos en mayúsculas
+muestra03$NOMBRE <-   toupper(muestra03$NOMBRE)
+
+# Pero antes intentaremos corregir los Jose´s porque puede haber conflicto con las Mari Jose´s
+# Primero probaremos con algunos nombres comunes de hombre
+muestra03$genero_imp[str_detect(muestra03$NOMBRE, pattern = "O$|EL$|JULIO|JOSE|CARLOS|DAVID|MANUEL|PABLO|PEDRO|JUAN|LUIS|CESAR")] <- "M"
+# Además de los finalizados por O (aunque podamos cargarnos alguno de mujer)
+
+# Añadimos Rosario y Rocio por lo del paso anterior
+muestra03$genero_imp[str_detect(muestra03$NOMBRE, pattern = "A$|AR$|MARIA|ANA|ROSARIO|ROCIO")] <- "F"
+
+# Muchos nombres vascos de chica acaban en NE
+muestra03$genero_imp[str_detect(muestra03$NOMBRE, pattern = "NE$|EN$")] <- "F"
+
+# Y los de chicos tienen tambien algunas terminaciones populares
+muestra03$genero_imp[str_detect(muestra03$NOMBRE, pattern = "ON$|OSU$|AITZ$|IKA$|ER$|OR$")] <- "M"
+
+length(muestra03$genero_imp)
+sum(is.na(muestra03$genero_imp))
+
+# Vemos que de 306085 entradas solo nos quedan sin imputar 58204, algo menos del 20%
+# pero tampoco hay que tomar mucho tiempo en este paso.
+
+
+
+
+
