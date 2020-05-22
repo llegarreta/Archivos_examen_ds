a23ea95ea32dc30f7699fbc42eecd5a300eca517
Nagore <nagore.bermeosolo@alumni.mondragon.edu>
2020-05-22 09:19:23 +0200

    Cambiamos nombres de variables

--- Cargafichero.R
+++ Cargafichero.R
@@ -1,3 +1,7 @@
 load("C:/Examen_DS/Archivos_examen_ds/muestra09.RData")
 dim(muestra09)
 #Tiene 307085 filas y 3 columnas
+
+#Cambiamos nombres
+colnames(muestra09)<-c("nif", "nombre", "genero")
+colnames(muestra09)
