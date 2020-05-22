ac131ee1b6bf3060510f03f837b014368894496e
maria <mariaepelde8@gmail.com>
2020-05-22 09:20:31 +0200

    Retocamos variable NIF

--- Carga fichero.R
+++ Carga fichero.R
@@ -4,6 +4,11 @@
 directorio <- setwd('C:/Users/auror/Desktop/RETOS/RETO 4/DS/Examen_DS/Archivos_examen_ds')
 load('muestra17.RData')
 dim(muestra17)
-colnames(muestra17) 
+colnames(muestra17) <- c('NIF', 'Nombre', 'Genero')
 str(muestra17)
 #El fichero tiene 310110 registros y 3 variables
+
+
+# Cambios en NIF ----------------------------------------------------------
+
+gsub(pattern="[[:punct:]]", muestra17$NIF, replacement=" ")
