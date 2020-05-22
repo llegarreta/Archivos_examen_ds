57bffe899be4fe0366ec825c572359c8f1965e75
maria <mariaepelde8@gmail.com>
2020-05-22 09:46:40 +0200

    Hemos reducido los registros que no cumplieran las condiciones del NIF

--- Carga fichero.R
+++ Carga fichero.R
@@ -12,3 +12,15 @@ str(muestra17)
 # Cambios en NIF ----------------------------------------------------------
 
 gsub(pattern="[[:punct:]]", muestra17$NIF, replacement=" ")
+#8 dígitos y una letra
+
+
+patron <- "([[:alpha:]]{1})"
+muestra17<-muestra17%>%
+  filter(grepl(pattern = patron, x = NIF))
+patron2 <- "([[:digit:]]{8})"
+muestra17<-muestra17%>%
+  filter(grepl(pattern = patron2, x = NIF))
+dim(muestra17)
+#Las dimensiones se han reducido a 282125 registros
+
