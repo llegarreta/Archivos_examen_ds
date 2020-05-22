bb6c968ebecbf7a94cd0e95661f4483933d39ead
irati <irati.garitano@alumni.mondragon.edu>
2020-05-22 10:07:29 +0200

    dimension 274904      3

--- Carga fichero.R
+++ Carga fichero.R
@@ -1,4 +1,18 @@
+library(stringi)
+library(stringr)
+library(rebus)
+
 load("C:/Users/irati/Desktop/Examen_DS/Archivos_examen_ds/muestra19.RData")
 dim(muestra19)
 
 colnames(muestra19)<-c("nif", "nombre","genero")
+
+
+# nif --------------------------------------------------------------------
+cambio <-START %R% "[0-9]"  %R% any_char(1,8) %R% "[A-Z]" %R% END
+muestra19$NIF<-str_extract(muestra19$nif, cambio)
+
+muestra19<-filter(muestra19,NIF!=is.na(NIF))
+muestra19<-muestra19[,1:3]
+
+dim(muestra19)
