9df4fa89a6199d85d77fcdf93335fa4502b18c50
adrian <adrian.martinez@alumni.mondragon.edu>
2020-05-22 09:31:41 +0200

    cambio de nombres de las variables

--- Carga fichero.R
+++ Carga fichero.R
@@ -1,3 +1,9 @@
 setwd("C:/Users/adria/Desktop/Examen_DS/Archivos_examen_ds")
 #Dimensiones de mi archivo
 dim(muestra38)
+
+
+#cambiar nombres
+names(muestra38)[1] ="nif"
+names(muestra38)[2] ="nombre"
+names(muestra38)[3] ="genero"
