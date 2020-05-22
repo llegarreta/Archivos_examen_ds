6a61c619d28cc85872e09f526b406bc7e892d8a8
Jie <jie.wu@alumni.mondragon.edu>
2020-05-22 10:46:32 +0200

    Intentar imputar los genero

--- Carga fichero.R
+++ Carga fichero.R
@@ -11,3 +11,21 @@ dim(muestra42) # Los datos tienen 300685filas y 3 variables
 
 # Cambiamos los nombres de las columnas -----------------------------------
 colnames(muestra42)<-c('nif','nombre','genero') #Mediante colnames cambiamos los nombres de las columnas
+
+# Crear variable nueva ----------------------------------------------------
+muestra42
+toupper(muestra42$nombre)
+unique(muestra42$nombre)
+library(tidyverse)
+
+muestra42$genero[muestra42$genero=='']<-NA
+sinna<-na.omit(muestra42)
+
+generos<-muestra42 %>%
+  mutate(genero_imp=paste(nombre,genero,sep = '_'))
+
+freqnombres<-generos %>% 
+  count(genero_imp) 
+
+generos$genero[generos$n>generos$n]="M"
+generos$genero[generos$n>generos$n]="V"
