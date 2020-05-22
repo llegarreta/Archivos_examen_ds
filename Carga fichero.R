a984c3ffb04a8a5e7e2f0f1c1bce3d8360c7c9e7
Iker Nieto <iker.nieto@alumni.mondragon>
2020-05-22 10:24:15 +0200

    Añadimos la imputación del género

--- Carga fichero.R
+++ Carga fichero.R
@@ -1,6 +1,15 @@
 load("C:/Users/ikern/Desktop/Examen_DS/Archivos_examen_ds/muestra30.RData")
 
 colnames(muestra30)
-names(muestra30)
 names(muestra30)<-c('nif','nombre','género')
+
+str(muestra30)
+
+library(stringi)
+library(stringr)
+library(rebus)
+
+
+muestra30$genero_imp<-0
+
 
