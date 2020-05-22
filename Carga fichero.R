7ecf39f2d2acb1423a33c38e6419d12288619fa7
Ane <ane.martinez@alumni.mondragon.edu>
2020-05-22 11:02:26 +0200

    Borramos el df de partida

--- Carga fichero.R
+++ Carga fichero.R
@@ -20,3 +20,38 @@ muestra29 <- muestra29%>%
   filter(nif_tf==T)
 
 muestra29 <- muestra29[,-4]
+<<<<<<< HEAD
+=======
+
+
+#genero
+chicos <- muestra29%>%
+  filter(genero=="V")%>%
+  group_by(nombre)%>%
+  count(nombre)
+
+chicas <- muestra29%>%
+  filter(genero=="M")%>%
+  group_by(nombre)%>%
+  count(nombre)
+
+vacios <- muestra29%>%
+  filter(genero=="")%>%
+  group_by(nombre)
+
+genero <- merge(muestra29, chicas, by="nombre")
+genero <- merge(genero, chicos, by="nombre")
+genero <- merge(genero, vacios, by="nombre")
+
+dput(colnames(genero))
+colnames(genero) <- c("nif", "nombre", "genero", "n_M", "n_V")
+
+
+genero$genero_imp[genero$n_M>genero$n_V]="M"
+genero$genero_imp[genero$n_V>genero$n_M]="V"
+
+genero <- genero%>%
+  filter(genero_imp!="")
+
+View(genero)
+>>>>>>> rama-genero
