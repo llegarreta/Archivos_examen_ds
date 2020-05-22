599e489b054efaee8eeecf1f03fcde8cbe5a650c
leire <leire.bartureni@alumni.mondragon.edu>
2020-05-22 09:13:14 +0200

    Dimendiones del fichero

---
+++ Carga fichero.R
@@ -0,0 +1,7 @@
+
+setwd("C:/Users/Leire/OneDrive - Mondragon Unibertsitatea/BDatA/Data Science/Introducción a Git-GitHub/Examen_DS/Archivos_examen_ds")
+df <- load("muestra07.RData")
+
+dim(muestra07)
+
+# Este fichero contiene 335086 filas y 3 columnas 
