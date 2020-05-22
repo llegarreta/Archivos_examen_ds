2fb34c6c6f04e18d72c8da79788d4529ee6b1d19
Endika <endika.diaz@alumni.mondragon.edu>
2020-05-22 10:22:28 +0200

    cambiamos nombre

---
+++ Cargamos fichero.R
@@ -0,0 +1,8 @@
+load("C:/Users/prueba/Desktop/Examen_DS/Archivos_examen_ds/Muestra16.Rdata")
+dim(muestra16)
+
+#El fichero tiene 3 variables y 299136 filas
+
+names(muestra16)[1]= "nif"
+names(muestra16) [2]= "nombre"
+names (muestra16) [3] = "genero"
