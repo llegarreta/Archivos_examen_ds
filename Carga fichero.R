51fec084c79e830d1ef88b045c77b03d3c3b364b
Jie <jie.wu@alumni.mondragon.edu>
2020-05-22 10:54:36 +0200

    Mediante un pattern buscar los nif que queremos y eliminar los que no queremos

--- Carga fichero.R
+++ Carga fichero.R
@@ -11,3 +11,13 @@ dim(muestra42) # Los datos tienen 300685filas y 3 variables
 
 # Cambiamos los nombres de las columnas -----------------------------------
 colnames(muestra42)<-c('nif','nombre','genero') #Mediante colnames cambiamos los nombres de las columnas
+
+
+# nif ---------------------------------------------------------------------
+
+cambio <- "([[:digit:]]{8})([[:alpha:]]{1})"
+grepl(pattern = cambio, x = muestra42$nif)
+muestra<-grepl(pattern = cambio, x = muestra42$nif)
+vector<-as.vector(which(muestra, arr.ind=FALSE))
+muestra42<-muestra42[c(vector),]
+
