9fbcc811744d9acbdee40a30c265c59f52fbbb5a
Jon <jon.amelibia@alumni.mondragon.edu>
2020-05-22 08:46:24 +0200

    Cargamos el fichero .R

---
+++ Carga fichero.R
@@ -0,0 +1,8 @@
+load("~/Desktop/Examen_DS/Archivos_examen_ds/muestra03.RData")
+
+str(muestra03)
+dim(muestra03)
+View(muestra03)
+
+# el fichero tiene 306085 filas y 3 columnas
+# DNI? Nombre y Género
