bcfdb031d4bcf03f1dbd449c898d791de3f8dd67
Unai Torrecilla <unai.torrecilla@alumni.mondragon.edu>
2020-05-22 09:13:35 +0200

    Cambio los nombres de las variables

--- Carga Fichero.R
+++ Carga Fichero.R
@@ -1,2 +1,8 @@
 load('muestra40.RData')
+library(dplyr)
 #El fichero tiene 339685 filas y 3 columnas
+
+muestra40 <- muestra40 %>% 
+  rename('nif' = 'var1',
+         'nombre' = 'var2',
+         'genero' = 'var3')
