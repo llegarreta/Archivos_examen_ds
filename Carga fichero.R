f909a0fe1183f02d95f30d7187d6427d2997f191
Aitor <aitor.hernandez@alumni.mondragon.edu>
2020-05-22 10:28:31 +0200

    cambios nif

--- Carga fichero.R
+++ Carga fichero.R
@@ -50,27 +50,15 @@ nif2 %>%
 # stri_subset (as.da(muestra21$nif,regex = funcion))
 # 
 # muestra21$nif[muestra21$nif == stri_subset(muestra21$nif,regex = funcion)]
-# 
-# 
-# cambio_nif <- "([[:digit:]]{8})([[:alpha:]]{1})"
+
+ cambiamos_de_nif <- "([[:digit:]]{8})([[:alpha:]]{1})"
 
 
-grepl(pattern = cambio_nif, x = muestra21$nif)
+grepl(pattern = cambiamos_de_nif, x = muestra21$nif)
 
-muestra2<-grepl(pattern = cambio_nif,  x = muestra21$nif)ç
+muestra2<-grepl(pattern = cambiamos_de_nif,  x = muestra21$nif)
 
-muestra3<-merge(muestra2, muestra21)
 vector<-as.vector(which(muestra2, arr.ind=FALSE))
 muestra21<-muestra21[c(vector),]
 
 
-
-
-
-
-
-
-
-
-
->>>>>>> rama-nif
