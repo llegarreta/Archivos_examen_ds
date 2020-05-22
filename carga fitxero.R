445b069e9adc7d755d135f2e093a36e8b5bec1cc
Cecilia <cecilia.bilbao@alumni.mondragon.edu>
2020-05-22 09:29:57 +0200

    cargamos un fitxero de r

---
+++ carga fitxero.R
@@ -0,0 +1,4 @@
+setwd("C:/Users/cecil/OneDrive/BUSINESS DATA ANALYTICS/PRIMERO/DATA SCIENCE/RETO 4/EXAMEN DS/Archivos_examen_ds")
+load("muestra10.RData", envir = parent.frame(), verbose = FALSE)
+dim(muestra10) #dimensiones 335686      3
+colnames(muestra10)
