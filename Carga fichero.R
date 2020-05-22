84c2891a7db2078d440ee0a9c584990b5b908df7
Iker <iker.cumplido@alumni.mondragon.edu>
2020-05-22 08:53:09 +0200

    Añado fichero R e inspecciono dimensiones

---
+++ Carga fichero.R
@@ -0,0 +1,14 @@
+
+# Cargamos datos ----------------------------------------------------------
+
+setwd("C:/Users/ikump/OneDrive/Escritorio/Examen_DS/Archivos_examen_ds")
+load("muestra14.RData")
+
+
+# Inspeccionamos dimensiones ----------------------------------------------
+
+dim(muestra14)
+#contiene 312110 filas y 3 columnas
+
+str(muestra14)
+#contiene 3 variables de tipo character
