7c4ec8e18f913d5908eee90fde0a50dfaa0cf59d
Cecilia <cecilia.bilbao@alumni.mondragon.edu>
2020-05-22 09:37:01 +0200

    añadimos nombre a las variables

--- carga fitxero.R
+++ carga fitxero.R
@@ -2,3 +2,8 @@ setwd("C:/Users/cecil/OneDrive/BUSINESS DATA ANALYTICS/PRIMERO/DATA SCIENCE/RETO
 load("muestra10.RData", envir = parent.frame(), verbose = FALSE)
 dim(muestra10) #dimensiones 335686      3
 colnames(muestra10)
+
+#cambiamos nombre de las variables
+names (muestra10)[1] = "nif"
+names (muestra10)[2] = "nombre"
+names (muestra10)[3] = "genero"
