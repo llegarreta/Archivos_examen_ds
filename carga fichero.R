ec95d6749239c6d65c59c12e280ade0bd5d11ca7
Sendoa Busquet <sendoabusquet@alumni.mondragon.edu>
2020-05-22 09:22:17 +0200

    cambio los nombres de las columnas

--- carga fichero.R
+++ carga fichero.R
@@ -1,3 +1,4 @@
 load("C:/Users/Usuario/Desktop/Examen_DS/Archivos_examen_ds/muestra11.RData")
 dim(muestra11)
 #Las dimensiones son 296686 filas y 3 columnas.
+colnames(muestra11)<-c("nif","nombre","genero")
