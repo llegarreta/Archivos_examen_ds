b8f7cc99d530fda6014b70038e5094a6f2f22abe
Aitor <aitor.hernandez@alumni.mondragon.edu>
2020-05-22 09:12:39 +0200

    cambiamos los nombres de las columnas

---
+++ Carga fichero.R
@@ -0,0 +1,6 @@
+setwd("C:/Users/ECI/Desktop/Examen_DS/Archivos_examen_ds")
+load("muestra21.RData")
+#tres variables, la primera numerica, la segunda con nombres, eventos o empresas, y
+#la tercera con el genero
+
+colnames(muestra21)<-c("nif","nombre","genero")
