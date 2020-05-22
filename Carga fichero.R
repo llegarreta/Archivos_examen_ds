8a266803483f8c7bee6c33357d1f841b28282eff
Dani <daniel.puente@alumni.mondragon.edu>
2020-05-22 09:31:10 +0200

    Modificamos el nif

--- Carga fichero.R
+++ Carga fichero.R
@@ -7,3 +7,21 @@ names(muestra35)[2] <- "Nombre"
 names(muestra35)[3] <- "genero"
 names(muestra35)
 
+View(muestra35)
+muestra35[muestra35 == ""] <- NA
+muestra35[muestra35 == 0] <- NA
+str(muestra35)
+
+regexp <- "([[:digit:]]{8})"
+a <- which(grepl(regexp, muestra35$nif)==T)
+a <- muestra35[a,]
+dim(a)
+a$nif <- substr(a$nif, 1,9)
+muestra35 <- a
+dim(muestra35)
+regexp <- "([A-Z]{1})"
+muestra35 <- muestra35[which(grepl(regexp, muestra35$nif)==T),]
+View(muestra35)
+
+
+
