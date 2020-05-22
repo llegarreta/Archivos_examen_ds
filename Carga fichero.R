96766e062d0882bb6c438a2048f9f7673891947e
Eneko <eneko.renteria@gmail.com>
2020-05-22 09:20:20 +0200

    Le ponemos nombre a las variables (este paso lo he añadido yo)

--- Carga fichero.R
+++ Carga fichero.R
@@ -1,3 +1,6 @@
 load('C:/Users/usuario/OneDrive/Escritorio/Examen_DS/Archivos_examen_ds/muestra36.RData')
  dim(muestra36)
-# El dataset muestra 36 contiene: 337685 filas y 3 columnas (variables)
+ # El dataset muestra 36 contiene: 337685 filas y 3 columnas (variables)
+ 
+colnames(muestra36) <- c('nif', 'nombre', 'genero')
+
