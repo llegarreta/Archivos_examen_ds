41ed058422e64cc297ebede64a787dff897186e5
ainhoa <ainhoaparedes2001@gmail.com>
2020-05-24 13:30:21 +0200

    eliminamos el dataset de partida de la carpeta

--- Carga fichero.R
+++ Carga fichero.R
@@ -6,4 +6,21 @@ View(muestra33)
 ## Nuestro fichero tiene 298685 filas y 3 columnas
 names(muestra33) <- c("nif", "nombre", "genero")
 names(muestra33)
+genero_imp=vector(mode='numeric', length=298685)
+genero_imp
 
+examen = data.frame(muestra33, genero_imp)
+View(examen)
+
+
+## Expresiones regulares 
+muestra33$var1(\n) #creamos una nueva fila
+muestra33$var2(\s) #dejamos un espacio
+muestra33$var3(\r)# volvemos al principio de la fila
+
+
+library(stringi)
+library(stringr)
+library(rebus)
+funcion = START %R% "[0-9]"  %R% any_char(1,8) %R% "[A-Z]" %R% END
+stri_subset(muestra33$nif,regex = funcion)
