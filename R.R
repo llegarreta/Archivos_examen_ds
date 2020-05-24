e742e3c70b87295df97f752e526805df825f0ba3
mario <m.rodriguez.angu@gmail.com>
2020-05-24 13:38:24 +0200

    guardamos archivos y cambiamos de rama

--- R.R
+++ R.R
@@ -12,11 +12,11 @@ View(muestra37)
 colnames(muestra37) <- c("nif","nombre","genero")
 View(muestra37)
 
-#Patrón de los nif de 8 letras y 1 numero
+#nifs con 8 letras y 1 numero
 patron = "([[:digit:]]{8})([[:alpha:]]{1})"
 nifs=str_extract(muestra37$nif, patron)
+
 #Lo añado a dataframe
 nifs=data.frame(nifs)
-#Uno la tabla a del patron a la muestra original
 muestra37=inner_join(nifs, muestra37, by = c("nifs" = "nif"))
 
