1b1e1a79cb3088837ea05222e734e8323daec417
ainhoa <ainhoaparedes2001@gmail.com>
2020-05-24 12:06:19 +0200

    Modifico carga fichero.R

---
+++ Carga fichero.R
@@ -0,0 +1,9 @@
+
+load("C:/Users/Ainhoa/Desktop/MU/Examen_DS/Archivos_examen_ds/muestra33.RData")
+dim(muestra33) 
+muestra33<- ("muestra33.RData")
+View(muestra33)
+## Nuestro fichero tiene 298685 filas y 3 columnas
+names(muestra33) <- c("nif", "nombre", "genero")
+names(muestra33)
+
