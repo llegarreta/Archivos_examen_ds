789b0379b28e29c74484c4960696ec251e439582
leire <leire.legarreta@gmail.com>
2020-05-27 11:59:04 +0200

    Modificamos el nombre de las vars en Carga Datos.R

--- carga datos.R
+++ carga datos.R
@@ -3,3 +3,5 @@ load("C:/Users/llegarreta/Desktop/Examen_ds/Archivos_examen_ds/muestra13.RData")
 dim(muestra13)
 
 # la muestra tiene 303136 registros y 3 variables
+
+colnames(muestra13)<-c("nif","nombre","genero")
