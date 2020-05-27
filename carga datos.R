59fb4e0fa1eb77dbc9192b4ed84388a65e040713
leire <leire.legarreta@gmail.com>
2020-05-27 12:11:29 +0200

    analizamos el nif en carga datos.R

--- carga datos.R
+++ carga datos.R
@@ -5,3 +5,9 @@ dim(muestra13)
 # la muestra tiene 303136 registros y 3 variables
 
 colnames(muestra13)<-c("nif","nombre","genero")
+
+# analisis genero
+muestra13<-muestra13[str_detect(muestra13$nif,"^([:digit:]{8})([:alpha:]{1})$"),]
+dim(muestra13)
+
+# El fichero tiene 275190 registros tras el análisis del nif
