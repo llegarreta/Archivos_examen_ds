a3e4515656aa9444465d5e96c010d7da555e9eca
Amaia <amaia.alcalde@alumni.mondragon.edu>
2020-06-16 00:18:28 +0200

    Transformaciones y depuraciones nif

--- Carga fichero.R
+++ Carga fichero.R
@@ -1,3 +1,14 @@
 load("C:/Users/amaia/OneDrive/Escritorio/1 BDATA/DATA SCIENCE/RECU DS/Archivos_examen_ds/muestra02.RData")
 #el fichero contiene 297111 instancias (filas) y 3 variables (columnas)
 colnames(muestra02)<- c("nif", "nombre", "genero")
+# PROCESAMIENTO NIF -------------------------------------------------------
+regexp <- "([[:digit:]]{8})([[:alpha:]]{1})"
+g <- as.vector(grepl(pattern = regexp, x = df$nif))
+df <- df %>% 
+  mutate(nif_imp = g)
+
+df <- df %>% 
+  filter(df$nif_imp == "TRUE")
+
+df <- df[,-5]
+
